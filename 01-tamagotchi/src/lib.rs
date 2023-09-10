04d7db7f45cf9baad7de42376783a05c74139794
magj2006 <magj2006@gmail.com>
2023-09-10 22:21:38 +0800

    complete 01-tamagotchi

--- 01-tamagotchi/src/lib.rs
+++ 01-tamagotchi/src/lib.rs
@@ -1,18 +1,44 @@
 #![no_std]
 
-use gstd::prelude::*;
+use gstd::{exec, msg, prelude::*};
+use tmg1_io::{Tamagotchi, TmgAction, TmgEvent};
+
+static mut STATE: Option<Tamagotchi> = None;
 
 #[no_mangle]
 extern "C" fn init() {
     // TODO: 5️⃣ Initialize the Tamagotchi program
+    let name: String = msg::load().expect("Error in loading name");
+    let date_of_birth = exec::block_height() as u64;
+    unsafe {
+        STATE = Some(Tamagotchi {
+            name,
+            date_of_birth,
+        })
+    };
 }
 
 #[no_mangle]
 extern "C" fn handle() {
     // TODO: 6️⃣ Add handling of `Name` and `Age` actions
+    let action: TmgAction = msg::load().expect("Error in loading TmgAction");
+    let state: &Tamagotchi = unsafe { STATE.as_ref().expect("failed to get contract state") };
+    match action {
+        TmgAction::Name => {
+            let name = state.name.clone();
+            msg::reply(TmgEvent::Name(name), 0).expect("Error in sending reply");
+        }
+        TmgAction::Age => {
+            let current_block_height = exec::block_height() as u64;
+            let age = current_block_height - state.date_of_birth;
+            msg::reply(TmgEvent::Age(age), 0).expect("Error in sending reply");
+        }
+    }
 }
 
 #[no_mangle]
 extern "C" fn state() {
     // TODO: 7️⃣ Return the Tamagotchi state
+    let state: &Tamagotchi = unsafe { STATE.as_ref().expect("failed to get contract state") };
+    msg::reply(state, 0).expect("Failed to share state");
 }
--- 01-tamagotchi/tests/smoke.rs
+++ 01-tamagotchi/tests/smoke.rs
@@ -1,10 +1,26 @@
-use gtest::{Program, System};
+use gtest::{Log, Program, System};
+use tmg1_io::{TmgAction, TmgEvent};
 
 #[test]
 fn smoke_test() {
     let sys = System::new();
     sys.init_logger();
-    let _program = Program::current(&sys);
+    let program = Program::current(&sys);
 
     // TODO: 8️⃣ Test the program initialization and message handling
+    let block_height_0 = sys.block_height();
+    let res = program.send(2, String::from("Hello"));
+    assert!(!res.main_failed());
+
+    // test `Name`
+    let res = program.send(2, TmgAction::Name);
+    let expected_log = Log::builder().payload(TmgEvent::Name(String::from("Hello")));
+    assert!(res.contains(&expected_log));
+
+    // test `Age`
+    let res = program.send(2, TmgAction::Age);
+    let block_height_1 = sys.block_height();
+    let age = block_height_1 - block_height_0;
+    let expected_log = Log::builder().payload(TmgEvent::Age(age.into()));
+    assert!(res.contains(&expected_log));
 }
